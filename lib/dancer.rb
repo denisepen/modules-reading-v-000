597ae018194d452c8168a0edd9a9f0681b34ec5a
Denise Pendleton <dpendl1@hotmail.com>
2018-02-28 19:01:43 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,6 +1,12 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
+include Dance
 class Dancer
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 
 end
